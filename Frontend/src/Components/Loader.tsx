e4fe193fa17b2365de70d6271a83cb5a5aefc11c
Harsh-DevOps117 <harsh.240103263@iiitbh.ac.in>
2025-06-23 19:47:11 +0530

    Update Loader.tsx

--- Frontend/src/Components/Loader.tsx
+++ Frontend/src/Components/Loader.tsx
@@ -1,4 +1,3 @@
-
 import React, { useEffect } from "react";
 import { createPortal } from "react-dom";
 
@@ -14,28 +13,18 @@ const FullScreenLoader: React.FC<FullScreenLoaderProps> = ({ isLoading }) => {
       document.body.classList.remove("no-scroll");
     }
 
-    
     return () => {
       document.body.classList.remove("no-scroll");
     };
-  }, [isLoading]); 
+  }, [isLoading]);
 
   if (!isLoading) {
-    return null; 
+    return null;
   }
 
   return createPortal(
-    <div
-      
-      
-      className="fixed inset-0 bg-black/10 backdrop-blur-md flex items-center justify-center z-[1000]"
-    >
-      <div
-        
-        className="w-20 h-20 border-4 border-t-4 border-t-black border-gray-200 rounded-full animate-spin-slow"
-      ></div>
-    
-      
+    <div className="fixed inset-0 bg-black/10 backdrop-blur-md flex items-center justify-center z-[1000]">
+      <div className="w-20 h-20 border-4 border-t-4 border-t-black border-gray-200 rounded-full animate-spin-slow"></div>
     </div>,
     document.body
   );
